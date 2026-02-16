161d2c1e929c397b4c6fe4d43693f96b2c246be9
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2026-02-16 11:41:49 +0100

    feat: implement UserClientsListManager for session and client management with JWT handling

---
+++ apps/bff/src/libs/userClientsListManager.ts
@@ -0,0 +1,112 @@
+import { logger } from "./logger";
+import jwt from 'jsonwebtoken';
+import { Session } from "../routers/api";
+
+export class UserClientsListManager {
+    sessionClients: Map<string, string[]>;
+    sessions: Map<string, Session>;
+
+    constructor() {
+        this.sessionClients = new Map();
+        this.sessions = new Map();
+    }
+
+    addUserSession(session : Session):void {
+        var obj = session;
+        if(obj.user && obj.user.jwt) {
+            obj.user.sso = jwt.decode(obj.user.jwt);
+        }
+        this.sessions.set(session.id, obj);
+        this.displaySessions();
+    }
+
+    displaySessions(): void {
+        logger.info("Sessions : {");
+        for (let [sessionId, sessionData] of this.sessions) {
+            logger.info("   " + sessionId + ":" + JSON.stringify(sessionData, null)+ ",");
+        }
+        logger.info("}");
+    }
+
+    getJWT(sessionId: string): string {
+        let result = this.sessions.get(sessionId);
+        if(result && result.user && result.user.jwt) {
+            return result.user.jwt;
+        } else {
+            throw new Error("Session not found or JWT not available for session " + sessionId);
+        }
+    }
+
+    getSession(sessionId: string) : Session {
+        let result = this.sessions.get(sessionId);
+        if(result) {
+            return result;
+        } else {
+            throw new Error("Session not found for sessionId " + sessionId);
+        }
+    }
+
+    refreshAuth(sessionId: string, access_token: string, refresh_token: string) : void {
+        var clientData = this.sessions.get(sessionId);
+        if(clientData && clientData.user) {
+            clientData.user.jwt = access_token;
+            clientData.user.sso = jwt.decode(access_token);
+            this.sessions.set(sessionId, clientData);
+        }
+    }
+
+    getRefreshClientList(sessions: string[]) : string[] {
+        let clientsToSend = [];
+        for (let i = 0; i < sessions.length; i++) {
+            let sessionId = sessions[i];
+            var clients = this.sessionClients.get(sessionId);
+            if(clients) {
+                for(let j = 0; j < clients.length; j++) {
+                    clientsToSend.push(clients[j]);
+                }
+            }
+        }
+        return clientsToSend;
+    }
+
+    removeSession(sessionId: string) : void {
+        this.sessionClients.delete(sessionId);
+        this.sessions.delete(sessionId);
+        this.displayClients();
+        this.displaySessions();
+    }
+
+    addClient(sessionId: string, clientId: string) : void {
+        var clients = this.sessionClients.get(sessionId);
+        if(clients) {
+            clients.push(clientId);
+            this.sessionClients.set(sessionId, clients);
+        } else {
+            this.sessionClients.set(sessionId, [ clientId ]);
+        }
+        this.displayClients();
+    }
+    
+    removeClient(sessionId: string, clientId: string) : void {
+        var clients = this.sessionClients.get(sessionId);
+        if(clients) {
+            var clientsFiltered = clients.filter(item => item !== clientId);
+            if(clientsFiltered.length == 0) {
+                this.sessionClients.delete(sessionId);
+            } else {
+                this.sessionClients.set(sessionId, clientsFiltered);
+            }
+        }
+        this.displayClients();
+    }
+
+    displayClients(): void {
+        logger.info("Clients : {");
+        for (let [sessionId, clientId] of this.sessionClients) {
+            logger.info("   " + sessionId + ":" + clientId+ ",");
+        }
+        logger.info("}");
+    }
+}
+
+export default new UserClientsListManager();
--- apps/bff/src/libs/usertools.ts
+++ apps/bff/src/libs/usertools.ts
@@ -1,240 +1,247 @@
-import { session } from "passport";
+import { Response } from "express";
+import { Session , AuthenticatedRequest } from "../routers/api";
 
-var jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+import { logger } from './logger';
+import config from '../config';
+import userClientsListManager from "./userClientsListManager";
 
-var passport = require('passport');
-
-let axios = require('axios');
-
-const logger = require('../../logger');
-const config = require('../../config');
-const userClientsListManager = require("./userClientsListManager");
-
-export class AuthentificationRequest extends Request {
-	user?: any;
-	session?: any;
-	originalUrl?: string;
+export function preTabs(level = 0): string {
+	var pre = '/';
+	for(var i = 0; i < level; i++){
+	  pre += '../';
+	}
+	return pre;
 }
 
-export function preTabs(level = 0) {
-		var pre = '/';
-		for(var i = 0; i < level; i++){
-		  pre += '../';
-		}
-		return pre;
-	}
+export function redirectOpenId(level : number = 0, req: AuthenticatedRequest, res: Response) : void {
+	var pre=preTabs(level);
+	return res.redirect(`${pre}users/openid`);
+}
 
-	export function redirectOpenId(level : number = 0, req: AuthentificationRequest, res: Response) {
-		var pre=preTabs(level);
-		//return res.redirect(`${pre}users/openid`);
-	}
-
-	export function auth(level: number = 0){
-		var pre=preTabs(level);
-		return function(req: AuthentificationRequest, res: Response, next: any) {
-		  let simvaToken = userClientsListManager.getJWT(req.session.id);
-		  if (req.session && req.session.user && req.session.user.jwt){
-			authExpiredAndRefreshAuthWithCallback(userClientsListManager.getSession(req.session.id), (error: Error, result: any) => {
-				if(error) {
-					req.session.intendedUrl=`${req.originalUrl}`;
-					//res.redirect(`${pre}users/login`); 
-				} else {
-					logger.debug("auth() - Token OK");
-					return next();
-				}
-			});
-		  } else if(simvaToken){
-			logger.info("auth() - New token");
-			let session = req.session;
-			let profile = getProfileFromJWT(simvaToken);
-			session.user.data = profile;
-			session.user.jwt = simvaToken;
-			userClientsListManager.addClient(session);
-			req.session.user.jwt = true;
-			logger.info("auth() - New token done");
-			return next();
-		  }else{
-			req.session.intendedUrl=`${req.originalUrl}`;
-			//res.redirect(`${pre}users/login`);
-		  }
-		};
-	}
-
-	export async function getRefreshSessionsList() {
-        let sessionsToSend = [];
-        for (let [sessionId, sessionData] of userClientsListManager.sessions) {
-            let ok = await isAuthExpiredPromise(sessionData.session);
-            if(ok) {
-                sessionsToSend.push(ok);
-            }
-        }
-        return sessionsToSend;
-    }
-
-    export async function isAuthExpiredPromise(session : any) {
-        return new Promise((resolve, reject) => {
-            isAuthExpired(session, (error: Error, result: any) => {
-                if(error) {
-                    reject(error);
-                } else {
-                    if(result.type == "expired") {
-                        resolve(session.id);
-                    }
-                }
-            });
-        });
-    }
-
-    export function authExpiredAndRefreshAuthWithCallback(session: any, callback: any) {
-        authExpired(session, config, (error: Error, result: any) => {
-            if(error) {
-                logger.info(JSON.stringify(error));
-				if(session && session.id) {
-             	   userClientsListManager.removeSession(session.id);
-				}
-                callback(error);
-            } else {
-                if(result) {
-                    logger.info(JSON.stringify(result));
-                    userClientsListManager.refreshAuth(session.id, result.access_token, result.refresh_token);
-                    logger.info("Auth Refreshed");
-                    callback(null, {message:"Auth Refreshed"});
-                } else {
-                    callback(null, {message:"Auth OK"});
-                }
-            }
-        });
-    }
-
-	export function setUser(req: AuthentificationRequest, user: any) {
-		let decoded = jwt.decode(user.jwt);
-		logger.info(`JWT : ${JSON.stringify(decoded)}`);
-		req.session.user.data.roles = decoded.realm_access.roles;
-		req.session.user.data.role = getRoleFromJWT(decoded);
-	}
-
-	export function isAuthExpired(session: any, callback: any){
-		try {
-			let current = Math.floor(Date.now() / 1000);
-			let jwtdecoded = decodeJWT(session.user.jwt);
-			let expiration = parseInt(jwtdecoded.exp);
-			if(current > expiration){
-				logger.info(`authExpired() - JWT: ${JSON.stringify(jwtdecoded)}`);
-				logger.info(`authExpired() - Expiration: ${expiration}`);
-				logger.info("authExpired() - Token Expired");
-				callback(null, {type:"expired"});
-			}else{
-				logger.debug("authExpired() - Token OK");
-				callback(null, {type:"ok"});
+export function auth(level: number = 0) : any {
+	var pre=preTabs(level);
+	return function(req: AuthenticatedRequest, res: Response, next: any) {
+	  let simvaToken = userClientsListManager.getJWT(req.session.id);
+	  if (req.session && req.session.user && req.session.user.jwt){
+		authExpiredAndRefreshAuthWithCallback(userClientsListManager.getSession(req.session.id), (error: Error, result: any) => {
+			if(error) {
+				req.session.intendedUrl=`${req.originalUrl}`;
+				return redirectOpenId(level, req, res);
+			} else {
+				logger.debug("auth() - Token OK");
+				return next();
 			}
-		} catch(e) {
+		});
+	  } else if(simvaToken){
+		logger.info("auth() - New token");
+		let session = req.session;
+		let profile = getProfileFromJWT(simvaToken);
+		if (session.user) {
+			session.user.sso = profile;
+			session.user.jwt = simvaToken;
+		}
+		userClientsListManager.addClient(session);
+		if (req.session.user) {
+			req.session.user.jwt = simvaToken;
+		}
+		logger.info("auth() - New token done");
+		return next();
+	  }else{
+		req.session.intendedUrl=`${req.originalUrl}`;
+		return redirectOpenId(level, req, res);
+	  }
+	};
+}
+
+export async function getRefreshSessionsList() : Promise<string[]> {
+     let sessionsToSend = [];
+     for (let [sessionId, sessionData] of userClientsListManager.sessions) {
+         let ok = await isAuthExpiredPromise(sessionData.session);
+         if(ok) {
+             sessionsToSend.push(ok);
+         }
+     }
+     return sessionsToSend;
+ }
+
+ export async function isAuthExpiredPromise(session : any) : Promise<string | null> {
+     return new Promise((resolve, reject) => {
+         isAuthExpired(session, (error: Error, result: any) => {
+             if(error) {
+                 reject(error);
+             } else {
+                 if(result.type == "expired") {
+                     resolve(session.id);
+                 }
+             }
+         });
+     });
+ }
+
+ export function authExpiredAndRefreshAuthWithCallback(session: any, callback: any): void {
+     authExpired(session, config, (error: Error, result: any) => {
+         if(error) {
+             logger.info(JSON.stringify(error));
+			if(session && session.id) {
+          	   userClientsListManager.removeSession(session.id);
+			}
+             callback(error);
+         } else {
+             if(result) {
+                 logger.info(JSON.stringify(result));
+                 userClientsListManager.refreshAuth(session.id, result.access_token, result.refresh_token);
+                 logger.info("Auth Refreshed");
+                 callback(null, {message:"Auth Refreshed"});
+             } else {
+                 callback(null, {message:"Auth OK"});
+             }
+         }
+     });
+ }
+
+export function setUser(req: AuthenticatedRequest, user: any): void {
+	let decoded = jwt.decode(user.jwt);
+	logger.info(`JWT : ${JSON.stringify(decoded)}`);
+	if (req.session?.user?.sso) {
+		req.session.user.sso.roles = decoded.realm_access.roles;
+		req.session.user.sso.role = getRoleFromJWT(decoded);
+	}
+}
+
+export function isAuthExpired(session: Session, callback: any): void {
+	try {
+		if (!session?.user?.jwt) {
 			callback({
 				status: 500,
 				data: {
 					message: 'Unable to parse accessToken',
-					error: e
+					error: 'Missing user or jwt token'
 				}
 			});
+			return;
 		}
-	}
-
-	export function authExpired(session: any, config: any, callback: any) {
-		isAuthExpired(session, (error: Error, result: any) => {
-			if(error) {
-				callback(error);
-			} else {
-				if(result.type == "expired") {
-					refreshAuth(session, config, callback);
-				} else {
-					callback();
-				}
+		let current = Math.floor(Date.now() / 1000);
+		let jwtdecoded = decodeJWT(session.user.jwt);
+		let expiration = parseInt(jwtdecoded.exp);
+		if(current > expiration){
+			logger.info(`authExpired() - JWT: ${JSON.stringify(jwtdecoded)}`);
+			logger.info(`authExpired() - Expiration: ${expiration}`);
+			logger.info("authExpired() - Token Expired");
+			callback(null, {type:"expired"});
+		}else{
+			logger.debug("authExpired() - Token OK");
+			callback(null, {type:"ok"});
+		}
+	} catch(e) {
+		callback({
+			status: 500,
+			data: {
+				message: 'Unable to parse accessToken',
+				error: e
 			}
 		});
 	}
+}
 
-	export function decodeJWT(token: string){
-		return jwt.decode(token);
-	}
+export function authExpired(session: any, config: any, callback: any): void {
+	isAuthExpired(session, (error: Error, result: any) => {
+		if(error) {
+			callback(error);
+		} else {
+			if(result.type == "expired") {
+				refreshAuth(session, config, callback);
+			} else {
+				callback();
+			}
+		}
+	});
+}
 
-	export function getProfileFromJWT(token: string){
-		let profile : any = {};
-		let simvaJwtToken = decodeJWT(token);
-		logger.info(`getProfileFromJWT() : ${JSON.stringify(simvaJwtToken)}`);
-		profile.provider = simvaJwtToken.iss;
-		profile.id = simvaJwtToken.data.id;
-		profile.username = simvaJwtToken.data.username;
-		profile.email = simvaJwtToken.email;
-		profile.roles = simvaJwtToken.realm_access.roles;
-		profile.role = getRoleFromJWT(simvaJwtToken);
-		return profile;
-	}
+export function decodeJWT(token: string): any {
+	return jwt.decode(token);
+}
 
-	export function getRoleFromJWT(decoded: any){
-		let role = 'norole';
-		if(decoded.realm_access.roles.includes('teacher') || decoded.realm_access.roles.includes('researcher')){
-			role = 'teacher';
-		} else if(decoded.realm_access.roles.includes('teaching-assistant') || decoded.realm_access.roles.includes('student')){
-			role = 'student';
-		};
-		return role;
-	}
+export function getProfileFromJWT(token: string): any {
+	let profile : any = {};
+	let simvaJwtToken = decodeJWT(token);
+	logger.info(`getProfileFromJWT() : ${JSON.stringify(simvaJwtToken)}`);
+	profile.provider = simvaJwtToken.iss;
+	profile.id = simvaJwtToken.data.id;
+	profile.username = simvaJwtToken.data.username;
+	profile.email = simvaJwtToken.email;
+	profile.roles = simvaJwtToken.realm_access.roles;
+	profile.role = getRoleFromJWT(simvaJwtToken);
+	return profile;
+}
 
-	export function refreshAuth(session: any, config: any, callback: any){
-		if(session.user && session.user.refreshToken){
-			logger.info(`refreshAuth() - Refresh Token : ${session.user.refreshToken}`);
-			const clientConfig= `${config.sso.clientId}:${config.sso.clientSecret}`;
-			const querystring = new URLSearchParams({
-				'grant_type': 'refresh_token',
-				'refresh_token': session.user.refreshToken
-			  });
-			axios.post(`${config.sso.url}/realms/${config.sso.realm}/protocol/openid-connect/token`, querystring, {
-				headers: {
-				  'Authorization': `Basic ${Buffer.from(clientConfig).toString('base64')}`,
-				  'Content-Type': 'application/x-www-form-urlencoded'
-				}
-			}).then((response: any) => {
-				try {
-					let simvaToken = response.data.access_token;
-					let simvaRefreshToken = response.data.refresh_token;
-					logger.debug(`refreshAuth() - Access Token : ${simvaToken}`);
-					logger.debug(`refreshAuth() - Refresh Token : ${simvaRefreshToken}`);
-					if(simvaToken == "undefined" || simvaToken == null) {
-						callback({
-							status: 500,
-							data: {
-								message: 'Token not active.',
-								error: response.data
-							}
-						});
-					} else {
-						callback(null, response.data);
-					}
-				} catch(e) {
-					logger.info(e);
+export function getRoleFromJWT(decoded: any) : string {
+	let role = 'norole';
+	if(decoded.realm_access.roles.includes('teacher') || decoded.realm_access.roles.includes('researcher')){
+		role = 'teacher';
+	} else if(decoded.realm_access.roles.includes('teaching-assistant') || decoded.realm_access.roles.includes('student')){
+		role = 'student';
+	};
+	return role;
+}
+
+export function refreshAuth(session: Session, config: any, callback: any) : void {
+	if(session.user && session.user.refreshToken){
+		logger.info(`refreshAuth() - Refresh Token : ${session.user.refreshToken}`);
+		const clientConfig= `${config.sso.clientId}:${config.sso.clientSecret}`;
+		const querystring = new URLSearchParams({
+			'grant_type': 'refresh_token',
+			'refresh_token': session.user.refreshToken
+		  });
+		axios.post(`${config.sso.url}/realms/${config.sso.realm}/protocol/openid-connect/token`, querystring, {
+			headers: {
+			  'Authorization': `Basic ${Buffer.from(clientConfig).toString('base64')}`,
+			  'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		}).then((response: any) => {
+			try {
+				let simvaToken = response.data.access_token;
+				let simvaRefreshToken = response.data.refresh_token;
+				logger.debug(`refreshAuth() - Access Token : ${simvaToken}`);
+				logger.debug(`refreshAuth() - Refresh Token : ${simvaRefreshToken}`);
+				if(simvaToken == "undefined" || simvaToken == null) {
 					callback({
 						status: 500,
 						data: {
-							message: 'Unable to refresh accessToken',
-							error: e
+							message: 'Token not active.',
+							error: response.data
 						}
 					});
+				} else {
+					callback(null, response.data);
 				}
-			})
-			.catch((error : Error) => {
+			} catch(e) {
+				logger.info(e);
 				callback({
 					status: 500,
 					data: {
 						message: 'Unable to refresh accessToken',
-						error: error
+						error: e
 					}
 				});
-			});
-		}else{
+			}
+		})
+		.catch((error : Error) => {
 			callback({
-				status: 401,
+				status: 500,
 				data: {
-					message: 'No user or refreshToken'
+					message: 'Unable to refresh accessToken',
+					error: error
 				}
 			});
-		}
+		});
+	}else{
+		callback({
+			status: 401,
+			data: {
+				message: 'No user or refreshToken'
+			}
+		});
 	}
+}
